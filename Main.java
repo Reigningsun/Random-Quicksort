3291a57e5a92c8c2e030c92da0bd23566b05db7f
Reigningsun <38302744+Reigningsun@users.noreply.github.com>
2018-06-08 16:50:33 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ import java.util.Random;
 public class Main {
 
 	
-	public static int[] swap(int[] array, int i, int j){										// Exchanges the values of i with the value of j
+	public static int[] swap(int[] array, int i, int j){									// Exchanges the values of i with the value of j
 		int temp = array[i];
 		array[i] = array[j];
 		array[j] = temp;
@@ -33,7 +33,7 @@ public class Main {
 	
 	
 	public static int random_partition(int[] toPartition, int lo, int hi){
-		Random rand = new Random();																// Create random number
+		Random rand = new Random();											// Create random number
 		int k = rand.nextInt((hi - lo) + 1) + lo;
 		
 		toPartition = swap (toPartition, lo, k);
@@ -44,16 +44,16 @@ public class Main {
 	
 	public static int[] random_quicksort(int[] toPartition, int lo, int hi){
 		if (lo < hi){
-			int p = random_partition(toPartition, lo, hi);										// Selects a random pivot 
-			random_quicksort(toPartition, lo, p - 1);											// Sorts the lower half of the partition
-			random_quicksort(toPartition, p + 1, hi); 											// Sorts the higher half of the partition
+			int p = random_partition(toPartition, lo, hi);								// Selects a random pivot 
+			random_quicksort(toPartition, lo, p - 1);								// Sorts the lower half of the partition
+			random_quicksort(toPartition, p + 1, hi); 								// Sorts the higher half of the partition
 		}
 		return toPartition;
 	}
 	
 	
 	
-	public static int random_select (int[] array, int i, int j, int median){					// Returns the index of the median value of an array in linear time
+	public static int random_select (int[] array, int i, int j, int median){						// Returns the index of the median value of an array in linear time
 		if (i < j){
 			int p = random_partition(array, i, j);
 			if (median == p){
@@ -74,7 +74,7 @@ public class Main {
 	
 	
 	//** Testing helpers ** 
-	public static void isSorted (int[] array){													// Checks that the array is actually sorted
+	public static void isSorted (int[] array){										// Checks that the array is actually sorted
 		boolean sorted = true;
 
 		for (int i = 0; i < array.length - 1; i++) {
@@ -88,7 +88,7 @@ public class Main {
 	
 	
 	
-	public static void contentsOfArray (int[] array){											// Prints out the contents of the array for inspection
+	public static void contentsOfArray (int[] array){									// Prints out the contents of the array for inspection
 		for (int i = 0; i < array.length; i++){
 			System.out.print(array[i] + " ");
 			if (i % 10 == 0){
@@ -116,34 +116,34 @@ public class Main {
 		int array[] = new int [100000];
 		Random rand = new Random();
 
-		for (int i = 0; i < array.length; i++){													// Fill the array with random int's between 10^6 and -10^6
+		for (int i = 0; i < array.length; i++){										// Fill the array with random int's between 10^6 and -10^6
 			array[i] = rand.nextInt((1000000 - -1000000) + 1) + -1000000;
 		}
 
 		
-		//contentsOfArray(array); 																// Outputs unsorted contents
+		//contentsOfArray(array); 											// Outputs unsorted contents
 		isSorted(array);
 		
 		System.out.println(" ");
 		
 		
-		long startTime = System.currentTimeMillis();											// Starting timer
+		long startTime = System.currentTimeMillis();									// Starting timer
 		array = random_quicksort(array, 0, array.length - 1);
-		long elapsedTime = (new Date()).getTime() - startTime;									// Calculate time of search
-		System.out.println("Time to sort 1st time: " + elapsedTime + " milliseconds");			// Print out the time taken
+		long elapsedTime = (new Date()).getTime() - startTime;								// Calculate time of search
+		System.out.println("Time to sort 1st time: " + elapsedTime + " milliseconds");					// Print out the time taken
 		isSorted(array);
 
 		
 		System.out.println(" ");
 		
 		
-		//contentsOfArray(array);																// Outputs sorted contents
+		//contentsOfArray(array);											// Outputs sorted contents
 		
 		
-		startTime = System.currentTimeMillis();													// Starting timer
+		startTime = System.currentTimeMillis();										// Starting timer
 		array = random_quicksort(array, 0, array.length - 1);
-		elapsedTime = (new Date()).getTime() - startTime;										// Calculate time of search
-		System.out.println("Time to sort 2nd time: " + elapsedTime + " milliseconds");			// Print out the time taken
+		elapsedTime = (new Date()).getTime() - startTime;								// Calculate time of search
+		System.out.println("Time to sort 2nd time: " + elapsedTime + " milliseconds");					// Print out the time taken
 		isSorted(array);
 		
 		
@@ -155,7 +155,7 @@ public class Main {
 		int array2[] = new int [100000];
 		Random rand2 = new Random();
 
-		for (int i = 0; i < array.length; i++){													// Fill the array with random int's between 10^6 and -10^6
+		for (int i = 0; i < array.length; i++){										// Fill the array with random int's between 10^6 and -10^6
 			array2[i] = rand2.nextInt((1000000 - -1000000) + 1) + -1000000;
 		}
 		
